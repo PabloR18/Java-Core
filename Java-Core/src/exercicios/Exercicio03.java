0524e2f682bfe2b7ab12e735491a0e3722071847
tci-dev <tci-dev@192.168.66.177>
2023-08-01 21:44:13 -0300

    Upload inicial

---
+++ Java-Core/src/exercicios/Exercicio03.java
@@ -0,0 +1,17 @@
+package exercicios;
+
+
+public class Exercicio03 {
+
+	public static void main(String[] args) {
+		int valor01 = 5;
+		int valor02 = 6;
+		int valor03 = 7;
+		int valor04 = 8;
+		
+		double diferenca = (valor01 * valor02 - valor03 * valor04);
+
+		System.out.printf("DIFERENCA = %.0f", diferenca);
+	}
+
+}
